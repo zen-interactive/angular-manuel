a2757238cb19ef5a6da831881ad2883a8b661c8c
wehrling <fwehrling@yahoo.fr>
2018-02-04 17:16:23 +0100

    first commit

---
+++ app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from "@angular/core";
+@Component({
+  selector: "app-ponyracer",
+  template: "<h1>PonyRacer</h1>"
+})
+export class PonyRacerComponent {}
---
+++ app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { PonyRacerComponent } from './app.component';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [PonyRacerComponent],
+  bootstrap: [PonyRacerComponent]
+})
+export class AppModule {} 
